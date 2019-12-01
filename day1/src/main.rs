a40b480b735ff1edfb6754f388dab62a0d27a649
darahak <dara.hak@gmail.com>
2019-12-01 22:55:56 +0100

    Simplify day 1 solution

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,21 +1,11 @@
 fn get_input() -> String {
-    use std::env;
     use std::fs;
 
-    let args: Vec<String> = env::args().collect();
-
-    if args.len() < 2 {
-        println!("Missing filename!");
-    }
-
-    let filename = &args[1];
-
-    return fs::read_to_string(filename).unwrap();
+    return fs::read_to_string("src/input.txt").unwrap();
 }
 
 fn main() {
     let contents = get_input();
-
     let mass_values: Vec<&str> = contents.split('\n').collect();
 
     let mut fuel_sum: i32 = 0;
