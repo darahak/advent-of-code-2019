a1acfa40078925189eedf2ac1563de5031dd2304
darahak <dara.hak@gmail.com>
2019-12-06 21:50:42 +0100

    Day 2 Part 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -4,21 +4,37 @@
 // 99: end of program
 fn main() {
     let contents = get_input();
-    let mut codes: Vec<usize> = contents
+    let codes: Vec<usize> = contents
         .trim()
         .split(',')
         .map(|s| s.parse::<usize>().unwrap())
         .collect();
 
-    run_opcodes(codes.as_mut_slice());
-
-    println!("Value at position 0: {}", codes[0]);
+    part1(codes.clone().as_mut_slice());
+    part2(&codes);
 }
 
-fn run_opcodes(opcodes: &mut [usize]) {
+fn part1(opcodes: &mut [usize]) {
     opcodes[1] = 12;
     opcodes[2] = 2;
 
+    let result = run_opcodes(opcodes);
+
+    println!("Result for part 1: {}", result);
+}
+
+fn part2(opcodes: &Vec<usize>) {
+    let expected: usize = 19690720;
+    let (noun, verb) = find_inputs_for_result(expected, &opcodes);
+
+    if noun == 0 && verb == 0 {
+        println!("Failed to find inputs for result {}", expected);
+    } else {
+        println!("Inputs for result {}: {} and {}", expected, noun, verb);
+    }
+}
+
+fn run_opcodes(opcodes: &mut [usize]) -> usize {
     let mut i = 0;
 
     while i < opcodes.len() {
@@ -39,11 +55,39 @@ fn run_opcodes(opcodes: &mut [usize]) {
             }
             99 => {
                 // End of program.
-                return;
+                break;
             }
             _ => println!("Unexpected opcode {} at position {}", opcodes[i], i),
         }
     }
+
+    return opcodes[0];
+}
+
+fn find_inputs_for_result(expected: usize, opcodes: &Vec<usize>) -> (usize, usize) {
+    let max: usize = 100;
+    let mut noun: usize = 0;
+    let mut verb: usize = 0;
+
+    for i in 0..max {
+        for j in 0..max {
+            let mut clone = opcodes.clone();
+            let program = clone.as_mut_slice();
+
+            program[1] = i;
+            program[2] = j;
+
+            let result = run_opcodes(program);
+
+            if result == expected {
+                noun = i;
+                verb = j;
+                break;
+            }
+        }
+    }
+
+    return (noun, verb);
 }
 
 fn get_input() -> String {
